d4ce2a61f8113a287359dd53c3964854f916ff56
Kayla-Joyner <86538615+Kayla-Joyner@users.noreply.github.com>
2021-07-25 00:20:24 -0400

    Update ppw.js
    trying to make navbar change colors too

--- ppw.js
+++ ppw.js
@@ -1,9 +1,12 @@
 function toggleDarkMode(){
   var bodyElement=document.getElementById("mainbackground");
+  var darkNavBar=document.getElementsbyClass("navbar");
   if (bodyElement.style.background=='sienna'){
     bodyElement.style.background='#cdb9ac';
+    darkNavBar.style.background='white';
   }
   else{
     bodyElement.style.background='sienna';
+    darkNavBar.style.background='#cdb9ac';
   }
 }
