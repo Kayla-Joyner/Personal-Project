cc32a42e3d6a0357a5f0c1a4014e0f218989c064
Kayla-Joyner <86538615+Kayla-Joyner@users.noreply.github.com>
2021-07-24 23:20:56 -0400

    Add files via upload
    Adding JavaScript so my dark mode button will work

---
+++ ppw.js
@@ -0,0 +1,9 @@
+function toggleDarkMode(){
+  var bodyElement=document.getElementById("mainbackground");
+  if (bodyElement.style.background=='black'){
+    bodyElement.style.background='#cdb9ac';
+  }
+  else{
+    bodyElement.style.background='black';
+  }
+}
