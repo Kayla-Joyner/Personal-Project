c0e22c79ecf5377cef37eb890bed390467a46e6e
Kayla-Joyner <86538615+Kayla-Joyner@users.noreply.github.com>
2021-07-25 12:32:41 -0400

    Update ppw.js
    trying to reverse changes

--- ppw.js
+++ ppw.js
@@ -1,12 +1,12 @@
 function toggleDarkMode(){
   var bodyElement=document.getElementById("mainbackground");
   var darkNavBar=document.getElementsByClassName("navbar")[0];
-  if (bodyElement.style.background=='darkTan'){
+  if (bodyElement.style.background=='sienna'){
     bodyElement.style.background='#cdb9ac';
     darkNavBar.style.background='white';
   }
   else{
-    bodyElement.style.background='darkTan';
+    bodyElement.style.background='sienna';
     darkNavBar.style.background='#cdb9ac';
   }
 }
