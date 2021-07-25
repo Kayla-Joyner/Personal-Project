b33a636559259148261853b85bfe161433829baa
Kayla-Joyner <86538615+Kayla-Joyner@users.noreply.github.com>
2021-07-25 12:31:46 -0400

    Update ppw.js
    trying to make the background a less orange brown

--- ppw.js
+++ ppw.js
@@ -1,12 +1,12 @@
 function toggleDarkMode(){
   var bodyElement=document.getElementById("mainbackground");
   var darkNavBar=document.getElementsByClassName("navbar")[0];
-  if (bodyElement.style.background=='sienna'){
+  if (bodyElement.style.background=='darkTan'){
     bodyElement.style.background='#cdb9ac';
     darkNavBar.style.background='white';
   }
   else{
-    bodyElement.style.background='sienna';
+    bodyElement.style.background='darkTan';
     darkNavBar.style.background='#cdb9ac';
   }
 }
