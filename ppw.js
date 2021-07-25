63055d3c4ae2e5c235fa5aedb29c799c359945aa
Kayla-Joyner <86538615+Kayla-Joyner@users.noreply.github.com>
2021-07-25 00:30:59 -0400

    Update ppw.js
    I think I needed to add an index number after the class name in line 3

--- ppw.js
+++ ppw.js
@@ -1,6 +1,6 @@
 function toggleDarkMode(){
   var bodyElement=document.getElementById("mainbackground");
-  var darkNavBar=document.getElementsByClassName("navbar");
+  var darkNavBar=document.getElementsByClassName("navbar")[0];
   if (bodyElement.style.background=='sienna'){
     bodyElement.style.background='#cdb9ac';
     darkNavBar.style.background='white';
