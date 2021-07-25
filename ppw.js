12a5568a3d3c7aec4b27697949fa08df13b88c14
Kayla-Joyner <86538615+Kayla-Joyner@users.noreply.github.com>
2021-07-25 12:44:37 -0400

    Update ppw.js
    trying hex code instead

--- ppw.js
+++ ppw.js
@@ -1,12 +1,12 @@
 function toggleDarkMode(){
   var bodyElement=document.getElementById("mainbackground");
   var darkNavBar=document.getElementsByClassName("navbar")[0];
-  if (bodyElement.style.background=='sienna'){
+  if (bodyElement.style.background=='#7b5d45'){
     bodyElement.style.background='#cdb9ac';
     darkNavBar.style.background='white';
   }
   else{
-    bodyElement.style.background='sienna';
+    bodyElement.style.background='#7b5d45';
     darkNavBar.style.background='#cdb9ac';
   }
 }
