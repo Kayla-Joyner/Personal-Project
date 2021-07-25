e32860679a2b0b9f7cd38c8b59f65fd9bdeccc7e
Kayla-Joyner <86538615+Kayla-Joyner@users.noreply.github.com>
2021-07-25 00:24:52 -0400

    Update ppw.js
    small error w/ camelCase this time

--- ppw.js
+++ ppw.js
@@ -1,6 +1,6 @@
 function toggleDarkMode(){
   var bodyElement=document.getElementById("mainbackground");
-  var darkNavBar=document.getElementsbyClassName("navbar");
+  var darkNavBar=document.getElementsByClassName("navbar");
   if (bodyElement.style.background=='sienna'){
     bodyElement.style.background='#cdb9ac';
     darkNavBar.style.background='white';
